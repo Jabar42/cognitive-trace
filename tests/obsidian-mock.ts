9140d608e74de28042fc7783c678f70ec5e739f3
jaime <jaimebarbudomier@gmail.com>
2026-07-20 00:28:40 -0500

    test: cubre controles del timeline

--- tests/obsidian-mock.ts
+++ tests/obsidian-mock.ts
@@ -1,3 +1,11 @@
 export class App {}
 export class PluginSettingTab {}
 export class Setting {}
+export class WorkspaceLeaf {}
+export class ItemView {
+    containerEl: any;
+
+    constructor(leaf: any) {
+        this.containerEl = leaf.containerEl;
+    }
+}
